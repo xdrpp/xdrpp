9bfc4867f26c8cdea76ad843940eb39be1c2d64d
David Mazieres <dm@uun.org>
2014-09-24 23:13:16 -0700

    undo thing that causes llvm bug

--- xdrc/gen_hh.cc
+++ xdrc/gen_hh.cc
@@ -515,6 +515,8 @@ gen(std::ostream &os, const rpc_union &u)
 	 << nl.close << "}";
   }
 
+#if 0
+  // Sadly, clang++ dumps core with this version
   top_material
     << "template<> struct xdr_traits<" << cur_scope()
     << "> : xdr_traits_base {" << endl
@@ -529,6 +531,19 @@ gen(std::ostream &os, const rpc_union &u)
     << " in " << cur_scope() << "\");" << endl
     << "    return size + 4;" << endl
     << "  }" << endl;
+#else
+  top_material
+    << "template<> struct xdr_traits<" << cur_scope()
+    << "> : xdr_traits_base {" << endl
+    << "  static constexpr bool is_class = true;" << endl
+    << "  static constexpr bool has_fixed_size = false;" << endl
+    << "  static std::size_t serial_size(const " << cur_scope()
+    << " &o) {" << endl
+    << "    case_serial_size ss;" << endl
+    << "    o._xdr_on_field_ptr(ss, &o, o._xdr_discriminant());" << endl
+    << "    return ss.size;" << endl
+    << "  }" << endl;
+#endif
   top_material
     << "  template<typename _Archive> static void" << endl
     << "  save(_Archive &_archive, const "
