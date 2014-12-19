24e25eea371b5c2fa80faef631fcefe1f425a8b4
David Mazieres <dm@uun.org>
2014-12-19 10:52:33 -0800

    make sure move constructor included, too.

--- xdrpp/types.h
+++ xdrpp/types.h
@@ -463,7 +463,7 @@ template<typename T> struct pointer : std::unique_ptr<T> {
   using std::unique_ptr<T>::get;
   pointer() = default;
   pointer(const pointer &p) : std::unique_ptr<T>(new T(*p)) {}
-  pointer &operator=(pointer &&) = default;
+  pointer(pointer &&p) = default;
   pointer &operator=(const pointer &up) {
     if (const T *tp = up.get()) {
       if (T *selfp = this->get())
@@ -475,6 +475,7 @@ template<typename T> struct pointer : std::unique_ptr<T> {
       this->release();
     return *this;
   }
+  pointer &operator=(pointer &&) = default;
   static void check_size(uint32_t n) {
     if (n > 1)
       throw xdr_overflow("xdr::pointer size must be 0 or 1");
